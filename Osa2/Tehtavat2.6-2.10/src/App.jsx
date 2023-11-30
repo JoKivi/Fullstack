9aa730b2daef7319be9276f6711212d7650475b6
JoKivi <joni.kivimaki@elisanet.fi>
2023-11-30 16:39:19 +0200

    2.10

--- Osa2/Tehtavat2.6-2.10/src/App.jsx
+++ Osa2/Tehtavat2.6-2.10/src/App.jsx
@@ -1,50 +1,96 @@
-import { useState } from 'react'
+import { useState } from 'react';
+
+const Filter = ({ filterName, handleFilterChange }) => (
+  <div>
+    filter shown with: <input type='text' value={filterName} onChange={handleFilterChange} />
+  </div>
+);
+
+const PersonForm = ({ newName, newNumber, handleNameChange, handleNumberChange, addPerson }) => (
+  <form onSubmit={addPerson}>
+    <div>
+      name: <input type='text' value={newName} onChange={handleNameChange} /><br />
+      number: <input type='number' value={newNumber} onChange={handleNumberChange} />
+    </div>
+    <div>
+      <button type="submit">add</button>
+    </div>
+  </form>
+);
+
+const Persons = ({ filteredPersons }) => (
+  <ul>
+    {filteredPersons.map((person) => (
+      <li key={person.name}>
+        {person.name}  {person.number}
+      </li>
+    ))}
+  </ul>
+);
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]);
+
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [filterName, setFilterName] = useState('');
 
   const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const handleFilterChange = (event) => {
+    setFilterName(event.target.value);
+  };
 
   const addPerson = (event) => {
     event.preventDefault();
 
-    const newPerson = { name: newName }
+    const newPerson = { name: newName, number: newNumber };
 
     if (newName.trim() === '' || persons.some(person => person.name === newName)) {
       alert('Nimi on tyhjä tai on jo luettelossa!');
       return;
     }
+    if (isNaN(newNumber)) {
+      alert('Numeroina ainoastaan!');
+      return;
+    }
 
-    setPersons([...persons, newPerson])
-    setNewName("")
-  }
+    setPersons([...persons, newPerson]);
+    setNewName('');
+    setNewNumber('');
+  };
+
+  const filteredPersons = persons.filter(person =>
+    person.name.toLowerCase().includes(filterName.toLowerCase())
+  );
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input type='text' value={newName} onChange={handleNameChange}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <ul>
-        {persons.map((person) => (
-          <li key={person.name}>{person.name}</li>
-        ))}
-      </ul>
+      <Filter filterName={filterName} handleFilterChange={handleFilterChange} />
+      <h3>Add a new</h3>
+      <PersonForm
+        newName={newName}
+        newNumber={newNumber}
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
+        addPerson={addPerson}
+      />
+      <h3>Numbers</h3>
+      <Persons filteredPersons={filteredPersons} />
     </div>
-  )
+  );
+};
 
-}
-
-export default App
-
+export default App;
